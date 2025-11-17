6e77271ffa17b29700abf086e3491e5f9529737b
unknown <debbieokon32@gmail.com>
2025-11-17 17:42:32 +0100

    made changes to swagger.js and installed swagger.json

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API documentation for the books project'
   },
   host: 'localhost:3000',
-  schemes: ['http', 'https']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
